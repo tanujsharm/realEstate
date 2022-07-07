2a45c005a2483ce9ca11b169f129bbb809f59fda
Tanuj Sharma <tanuj.khandel06@gmail.com>
2022-07-07 20:14:16 +0530

    fixed footer and header

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import { Box } from "@chakra-ui/react";
 
-// import Footer from "./Footer";
-// import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
   return (
@@ -11,9 +11,11 @@ export default function Layout({ children }) {
         <title>Real Estate</title>
       </Head>
       <Box maxWidth='1280px' m='auto'>
-        <header>{/* <Navbar /> */}</header>
+        <header>
+          <Navbar />
+        </header>
         <main>{children}</main>
-        <footer>{/* <Footer /> */}</footer>
+        <footer>{<Footer />}</footer>
       </Box>
     </>
   );
